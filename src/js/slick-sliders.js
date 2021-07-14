4a5168038aa9cbeba7b1163d2e0c4a2a60e05b16
Nina Antoniuk <Antoniuk_Nina@ukr.net>
2021-07-14 09:50:20 +0300

    all markap

--- src/js/slick-sliders.js
+++ src/js/slick-sliders.js
@@ -24,3 +24,31 @@ $('.slider').slick({
     }
   ]
 });
+
+$('.goods__slider').slick(
+  {
+  centerMode: true,
+  arrows: false,
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+}
+);
