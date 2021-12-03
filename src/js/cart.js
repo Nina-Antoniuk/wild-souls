eafa20d0220f80611b02a25296996b700daa4c40
Nina <ninar313@gmail.com>
2021-12-04 00:17:46 +0200

    cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,60 +1,111 @@
+import { v4 as uuid } from 'uuid';
 import refs from './refs';
 
 refs.goodsListEl.addEventListener('click', createProductItemHandler);
 refs.cartBtnEl.addEventListener('click', opensCartHandler);
 refs.mobileCartBtnEl.addEventListener('click', opensCartHandler);
+refs.cartSection.addEventListener('click', clickHandler);
 
 const cart = {
   items: [],
 
-  addItem(product) {
-    createCartItemMurkup(product);
-    this.items.push(product);
+  add(data) {
+    this.items.push(data);
   },
 
-  setPrice(product) {
-    return Number(product.price) * product.quantity;
+  decrement(e, id) {
+    this.items.find(el => {
+      if (el.id === id) {
+        if (el.quantity > 1) {
+          el.quantity -= 1;
+          this.chengeQuantityMarkup(e, el);
+          el.price = (el.price / (el.quantity + 1)) * el.quantity;
+          this.chengePriceMarkup(e, el);
+        } else {
+          const btn = document.querySelector(`#${el.btn}`);
+          btn.removeAttribute('disabled');
+          e.target.parentNode.remove();
+        }
+      }
+    });
+  },
+
+  increment(e, id) {
+    return this.items.find(el => {
+      if (el.id === id) {
+        el.quantity <= 0 ? el.quantity : (el.quantity += 1);
+        this.chengeQuantityMarkup(e, el);
+        el.price = (el.price / (el.quantity - 1)) * el.quantity;
+        this.chengePriceMarkup(e, el);
+      }
+    });
+  },
+
+  createMarkup({ id, title, quantity, price }) {
+    const data = `
+      <li id=${id}>
+        ${title}
+        <button class="decrement" type="button"> - </button>
+        <span class="quantity">${quantity}</span>
+        <button class="increment" type="button"> + </button>
+        total price
+        <span class="total-price">${price}</span> &#8364;
+      </li>`;
+
+    refs.shoppingListEL.insertAdjacentHTML('beforeend', data);
+  },
+
+  chengeQuantityMarkup(e, el) {
+    e.target.parentNode.children[1].textContent = el.quantity;
+  },
+
+  chengePriceMarkup(e, el) {
+    e.target.parentNode.children[3].textContent = el.price.toFixed(2);
   },
 
   clear() {
     cart.items = [];
+    console.log(refs.goodsListEl); // on delete + remove all disabled attr from btn`s on product cards
   },
 };
 
-function createProduct(targetBtnParentEl) {
-  const product = {};
-  product.title = targetBtnParentEl.children[0].textContent;
-  product.price = targetBtnParentEl.children[1].children[0].textContent;
-  product.quantity = 1;
-  return product;
-}
+function clickHandler(e) {
+  const targetElClassList = [...e.target.classList];
+  const itemId = e.target.parentNode.id;
+  if (targetElClassList.includes('increment')) {
+    cart.increment(e, itemId);
+  }
 
-function createCartItemMurkup(product) {
-  const markup = `<li>${product.title} 
-                    <span id="quantity">${product.quantity}</span>
-                    - total price
-                    <span id="total-price">${cart.setPrice(product)}</span> &#8364;
-                  </li>`;
-  refs.shoppingListEL.insertAdjacentHTML('beforeend', markup);
+  if (targetElClassList.includes('decrement')) {
+    cart.decrement(e, itemId);
+  }
 }
 
 function createProductItemHandler(e) {
   if (e.target.nodeName !== 'BUTTON') {
     return;
   } else {
+    e.target.setAttribute('disabled', true);
     const targetBtnParentEl = e.target.parentNode;
     const product = createProduct(targetBtnParentEl);
-    cart.addItem(product);
+    cart.add(product);
+    cart.createMarkup(product);
   }
+}
+
+function createProduct(targetBtnParentEl) {
+  const product = {};
+  product.title = targetBtnParentEl.children[0].textContent;
+  product.price = targetBtnParentEl.children[1].children[0].textContent;
+  product.quantity = 1;
+  product.id = uuid();
+  product.btn = targetBtnParentEl.children[2].id;
+  return product;
 }
 
 function opensCartHandler() {
   refs.cartSection.classList.remove('is-hidden');
   refs.cartCloseBtnEl.addEventListener('click', closesCartHandler);
-
-  const goodsListEl = document.querySelector('.goods-list-js');
-
-  goodsListEl.addEventListener('click', createProductItemHandler);
 }
 
 function closesCartHandler() {
